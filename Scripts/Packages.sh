83b32521629e4ef1ed55e99078662f539c280994
haixing1001 <50311834+haixing1001@users.noreply.github.com>
2025-08-13 00:57:14 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -53,7 +53,7 @@ UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
-UPDATE_PACKAGE "mypackages" "haixing1001/my-packages" "main" "" "cdnspeedtest luci-app-cloudflarespeedtest"
+UPDATE_PACKAGE "cdnspeedtest" "kenzok8/small-package" "main" "" "cdnspeedtest luci-app-cloudflarespeedtest"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
